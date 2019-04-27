0ca5b3257147cca29db03c55954302a5b015bd54
Piyush Mehta <me@piyushmehta.com>
2019-04-27 22:22:01 +0530

    cors policty

--- server/app.js
+++ server/app.js
@@ -1,46 +1,53 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var utils = require('./routes/utils');
-var dauth = require('./routes/dauth');
-var verifier = require('./routes/verifier');
-
+var index = require("./routes/index");
+var users = require("./routes/users");
+var utils = require("./routes/utils");
+var dauth = require("./routes/dauth");
+var verifier = require("./routes/verifier");
 
 var app = express();
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 console.log("HERE");
 mongoose.connect("mongodb://localhost:27017");
 console.log("HERE2");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 console.log("HERE3");
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 console.log("HERE4");
-app.use('/', index);
+app.use("/", index);
 console.log("HERE5");
-app.use('/users', users);
-app.use('/utils',utils);
-app.use('/dauth',dauth);
-app.use('/verifier',verifier);
+app.use("/users", users);
+app.use("/utils", utils);
+app.use("/dauth", dauth);
+app.use("/verifier", verifier);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -49,12 +56,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-
 
 module.exports = app;
